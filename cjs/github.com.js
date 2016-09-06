446be1bb64e23c0e03f521e0a5d2681483beb4d3
Artur Klesun <ArturKlesun@gmail.com>
2016-09-06 23:49:01 +0300

    Update github.com.js

--- cjs/github.com.js
+++ cjs/github.com.js
@@ -1,8 +1,25 @@
 var $$ = s => Array.from(document.querySelectorAll(s));
 
-var rawGitUrl = 'https://rawgit.com' + window.location.pathname.replace('/blob/master/', '/master/');
-setTimeout(
-    () => $$('.file-header').forEach(el => 
-        el.innerHTML += '<br clear="all"/><a href="' + rawGitUrl + '">rawgit</a>'),
-    500
+var addLinkToRawgit = function()
+{
+    var rawGitUrl = 'https://rawgit.com' + window.location.pathname.replace('/blob/master/', '/master/');
+    setTimeout(
+        () => $$('.file-header').forEach(el => 
+            el.innerHTML += '<br clear="all"/><a href="' + rawGitUrl + '">rawgit</a>'),
+        500
+    );
+};
+
+addLinkToRawgit();
+var pathname = window.location.pathname;
+setInterval(
+	() => {
+		// youtube does not actually reload the page when you play next video
+		if (pathname != window.location.pathname) {
+			pathname = window.location.pathname;
+			addLinkToRawgit();
+		}
+	},
+	500
 );
+
